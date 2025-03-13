6c07959b8281d47a60f444464a525b3cc0b715a7
Jenny Tan <jennytan817@gmail.com>
2025-03-13 21:54:30 +0800

    Create Transportation class
    Add attributes, constructor, getters and setters.

---
+++ src/main/java/VoyaTrip/Transportation.java
@@ -0,0 +1,28 @@
+package VoyaTrip;
+
+public class Transportation {
+    private String mode;
+    private Integer budget;
+
+    public Transportation(String mode, Integer budget) {
+        this.mode = mode;
+        this.budget = budget;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public Integer getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Integer budget) {
+        this.budget = budget;
+    }
+
+}
