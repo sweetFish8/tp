cd8f8c8603e06ca7f85d57c95f4de8f435dd6c05
Jenny Tan <jennytan817@gmail.com>
2025-03-16 16:16:28 +0800

    Remove transportationCount
    The transportation count can be directly obtained from the size() method from the ArrayList

--- src/main/java/voyatrip/Trip.java
+++ src/main/java/voyatrip/Trip.java
@@ -6,23 +6,19 @@ import java.util.ArrayList;
 public class Trip {
 
     private ArrayList<Transportation> transportations;
-    private Integer transportationCount;
 
     public Trip() {
         this.transportations = new ArrayList<Transportation>();
-        this.transportationCount = 0;
     }
 
     public void addTransportation(String transportMode) {
         transportations.add(new Transportation(transportMode));
-        transportationCount++;
     }
 
     public void deleteTransportation(String transportMode) {
         for (Transportation transportation : transportations) {
             if (transportation.getMode().equals(transportMode)) {
                 transportations.remove(transportation);
-                transportationCount--;
             }
         }
 
