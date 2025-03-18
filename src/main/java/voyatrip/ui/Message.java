fad2788df9f493aea76aed938c3de984de927a25
Lyam-T <e1496889@u.nus.edu>
2025-03-18 21:01:05 +0800

    Restructure main logic

--- src/main/java/voyatrip/ui/Message.java
+++ src/main/java/voyatrip/ui/Message.java
@@ -1,11 +1,9 @@
 package voyatrip.ui;
 
 public class Message {
+    // user message
+    static final String WELCOME_MESSAGE = "Welcome to VoyaTrip! How can I help you today?";
+    static final String GOODBYE_MESSAGE = "Goodbye! Wish you have a lovely trip!";
     // exception
-    private static final String INVALID_COMMAND_MESSAGE = "Invalid command. Please try again.";
-
-    // exception
-    public static void printInvalidCommand() {
-        System.out.println(INVALID_COMMAND_MESSAGE);
-    }
+    static final String INVALID_COMMAND_MESSAGE = "Invalid command. Please try again.";
 }
