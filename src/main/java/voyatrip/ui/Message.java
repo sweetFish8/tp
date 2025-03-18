d9e1d82078c41190f6b4854879f160c5486d5652
Jenny Tan <jennytan817@gmail.com>
2025-03-18 21:56:11 +0800

    Add printTripNotFound message

--- src/main/java/voyatrip/ui/Message.java
+++ src/main/java/voyatrip/ui/Message.java
@@ -3,9 +3,14 @@ package voyatrip.ui;
 public class Message {
     // exception
     private static final String INVALID_COMMAND_MESSAGE = "Invalid command. Please try again.";
-
+    private static final String TRIP_NOT_FOUND_MESSAGE = "Trip not found. Please try again.";
     // exception
     public static void printInvalidCommand() {
         System.out.println(INVALID_COMMAND_MESSAGE);
+    }
+
+    //exception
+    public static void printTripNotFound() {
+        System.out.println(TRIP_NOT_FOUND_MESSAGE);
     }
 }
